356b720682ba68132307134a22485e2163086261
Yukihiko SAWANOBORI <sawanoboriyu@gmail.com>
2019-01-12 15:18:22 +0900

    Create get.sh

---
+++ get.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+
+
+wget -O 68efb5c1-7ff1-4de5-be14-a97cb3bce9bf.tgz "https://wp-archives-files.s3.amazonaws.com/76c7020c-a84f-11e7-94b6-b64fb4ef4b4d/68efb5c1-7ff1-4de5-be14-a97cb3bce9bf.tgz?AWSAccessKeyId=ASIAQMWNEJAHLKXWL65J&Expires=1547276439&Signature=cUuC07cfJdjVgBzDnRAuUT4Lvjg%3D&X-Amzn-Trace-Id=Root%3D1-5c398286-2130715206eb84f88dc96730%3BParent%3Df3c15c72f9068e93%3BSampled%3D1&x-amz-security-token=FQoGZXIvYXdzEHMaDKKCGt6WOO7NcqGp7iL%2FAasm4PvWit8I31hsOjzxSvw9r7E%2F0Mgo3iALw1ABX7MIgvJI2Ct2wE%2F0vVnIt1c%2Bdcu4P5rRuLEDQQhkUVHuV7gNIXm2TQyphr%2BWTA4%2BPmuFZIwCTlePRU4Wb%2BD2ItDwRuFll0d7cb54rcMsorCiCDttjGmWq8PAjfGlQBeS%2BMwgLKk7T18WhSYgvkysD2EYcTwwkIKZwTeTmkDDw%2BF29PXL%2F6GiS4oh5P3C16gKIlCn0J0NVv486KTaiWjcgOihd%2F7%2FLIObybZQecfXgEvydcc5YSDSTEm%2FBnPAkSCNgnPERAgevLWzwQA3ALkGbc6TFuIg7G3IClmsEaAdckkl1yjEiuXhBQ%3D%3D"
+tar xvzf 68efb5c1-7ff1-4de5-be14-a97cb3bce9bf.tgz
+mv 68efb5c1-7ff1-4de5-be14-a97cb3bce9bf public
