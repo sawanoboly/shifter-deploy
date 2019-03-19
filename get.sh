f1add3a1cacf4876fdee2bc6eaac034839343abc
Yukihiko SAWANOBORI <sawanoboriyu@gmail.com>
2019-03-19 19:50:18 +0900

    Update get.sh

--- get.sh
+++ get.sh
@@ -7,8 +7,8 @@ if [ "$INCOMING_HOOK_BODY" == "" ] ; then
   exit 0
 fi
 
-DOWNLOAD_URL=`echo $INCOMING_HOOK_BODY | jq -r .DOWNLOAD_URL | base64 -d`
-ARTIFACT_ID=`echo $INCOMING_HOOK_BODY | jq -r .ARTIFACT_ID`
+DOWNLOAD_URL=`echo $INCOMING_HOOK_BODY | jq -r .download_url | base64 -d`
+ARTIFACT_ID=`echo $INCOMING_HOOK_BODY | jq -r .artifact_id`
 
 wget -O "$ARTIFACT_ID.tgz" "$DOWNLOAD_URL"
 tar xvzf $ARTIFACT_ID.tgz
